6bbb14422f331ccdba764ecebdf87751d405870d
George Witteman <georgewitteman@gmail.com>
2018-05-19 22:22:22 -0400

    Add script to copy appropriate files to submission folder

---
+++ final-project/cp-submission.sh
@@ -0,0 +1,12 @@
+mkdir -p ./submission
+cp Block.m ./submission/Block.m
+cp Camera.m ./submission/Camera.m
+cp feetToInches.m ./submission/feetToInches.m
+cp inchesToFeet.m ./submission/inchesToFeet.m
+cp main.m ./submission/main.m
+cp proposal.pdf ./submission/proposal.pdf
+cp randMinMax.m ./submission/randMinMax.m
+cp Robot.m ./submission/Robot.m
+cp SimulationParameters.m ./submission/SimulationParameters.m
+cp squareVertices.m ./submission/squareVertices.m
+cp write-up.pdf ./submission/gewitteman-writeup.pdf
